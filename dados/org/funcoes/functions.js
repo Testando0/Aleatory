36bca89c776d713a48ab0c801c599e9d1ab7eb3d
AleatoryConteudos <josivalribeiro@gmail.com>
2024-11-08 20:05:15 -0300

    Atualizando/Corrigindo.

--- dados/org/funcoes/functions.js
+++ dados/org/funcoes/functions.js
@@ -122,7 +122,7 @@ colors: [`${cor3}`,`${cor1}`,`${cor5}`],
 lineHeight: 1
 });
  
-const banner3 = cfonts.render((`ALEATORY MD\n4.1`), {
+const banner3 = cfonts.render((`ALEATORY MD\n4.5`), {
 font: 'slick',             
 align: 'center',           
 colors: [`${cor1}`,`${cor3}`,`${cor4}`,`${cor2}`],
--- start.sh
+++ start.sh
@@ -6,8 +6,6 @@ echo -e "${GREEN} Aleatory - Auto reconexão ativada para prevenção de quedas.
 
 if [ "$1" = "sim" ]; then
 node iniciar.js sim
-elif [ "$1" = "não" ]; then
-node iniciar.js não
 else 
 node iniciar.js
 fi
